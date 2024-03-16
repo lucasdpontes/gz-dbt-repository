d672ded34baa73a92a7c30797f8527aab1d2f9e0
dpontes.lucas@gmail.com <dpontes.lucas@gmail.com>
2024-03-16 20:45:15 +0000

    tables created

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,10 @@
+SELECT
+p.products_id
+,p.purchase_price AS purchase_price
+,CAST(s.quantity AS INT64) * CAST(p.purchase_price AS FLOAT64) AS purchase_cost
+,ROUND(s.revenue - s.revenue*CAST(p.purchase_price AS FLOAT64),2) AS margin
+FROM {{ ref('stg_raw__product')}} p
+LEFT JOIN {{ ref('stg_raw__sales')}} s ON p.products_id = s.products_id
+
+--Margin = Revenue - Purchase_cost
+--Purchase_cost = Quantity * Purchase_price
