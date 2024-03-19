2e04a459c2b03561e2628a34a9a147c130c9d141
Lucas Pontes <dpontes.lucas@gmail.com>
2024-03-19 23:56:08 +0000

    correction of queries

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,11 +1,11 @@
 SELECT
-s.orders_id
+o.orders_id
 ,o.date_date
 ,ROUND((o.margin + s.shipping_fee - s.log_cost - CAST(s.ship_cost AS FLOAT64)),2) AS operational_margin
 ,o.quantity
 
-FROM {{ ref('stg_raw__ship')}} s
-LEFT JOIN {{ ref('int_orders_margin')}} o USING (orders_id)
+FROM {{ ref('int_orders_margin')}} o
+LEFT JOIN {{ ref('stg_raw__ship')}} s USING (orders_id)
 
 ORDER BY orders_id DESC
 
