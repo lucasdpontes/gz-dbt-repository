7b16a6ac309cc2d36a47ef93b41e89338c26bce4
Lucas Pontes <dpontes.lucas@gmail.com>
2024-03-20 00:13:25 +0000

    adjustments

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,10 +1,10 @@
 SELECT
 date_date
 ,COUNT(orders_id) AS nb_transactions
-,SUM(revenue)
-,AVG(revenue) AS average_basket
-,SUM(margin)
-,SUM(operational_margin)
+,ROUND(SUM(revenue),2) AS revenue
+,ROUND(AVG(revenue),2) AS average_basket
+,ROUND(SUM(margin),2) AS margin
+,ROUND(SUM(operational_margin),2) AS operational_margin
 
 FROM {{ ref('int_orders_operational')}}
 
